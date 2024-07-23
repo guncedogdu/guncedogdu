2a4701160b0beae737596d32ace48ef86bf81409
guncedogdu <ylmzselcukk@gmail.com>
2024-07-23 23:42:57 +0300

    asdasd

--- src/Pages/Games.jsx
+++ src/Pages/Games.jsx
@@ -1,19 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
 
 const Games = () => {
-  const items = 
-  [
-    {game:"https://html5.gamedistribution.com/a55c9cc9c21e4fc683c8c6857f3d0c75/?timestamp=-62135596800&amp;countryCode=tr&amp;siteid=88&amp;channelid=1&amp;siteLocale=tr&amp;locale=tr&amp;gd_sdk_referrer_url=https%3A%2F%2Fwww.oyunskor.com%2Foyun%2Fates-ve-su"}
-  ]
+  const items = [
+    {gameName:"Ateş ve Su", game: "https://html5.gamedistribution.com/a55c9cc9c21e4fc683c8c6857f3d0c75/?timestamp=-62135596800&amp;countryCode=tr&amp;siteid=88&amp;channelid=1&amp;siteLocale=tr&amp;locale=tr&amp;gd_sdk_referrer_url=https%3A%2F%2Fwww.oyunskor.com%2Foyun%2Fates-ve-su"},
+    {gameName:"Kory the Thief", game: "https://yombie.com/especial/kory-the-thief.swf/kory-the-thief"},
+    {gameName:"Barmen", game: "https://bartender-the-right-mix.game-files.crazygames.com/ruffle/bartendertherightmix.html?v=1.294"},
+    {gameName:"Ultimate Flash Sonic", game: "https://www.gamenora.com/upload/games/swf/SonicFlash/"},
+    {gameName:"Regular Show Ultimate Fist Punch", game: "https://emea.iframed.cn.dmti.cloud/content/344/game/fist-punch/uk/fistpunch-170818/index.html?pageName=cn/regular-show-fist-punch&gametitle=Fist%20Punch&server=web|www.cartoonnetwork.co.uk&OnetrustActiveGroups=,req,BG2373,"},
+    {gameName:"Home Sheep Home", game: "https://www.gameflare.com/embed/home-sheep-home/"},
+    {gameName:"Penguin Diner", game: "https://html5.gamedistribution.com/dd108118c3aa426e88fe2f7baebfd72b/?gd_sdk_referrer_url=https%3A%2F%2Fwww.crazygames.com%2Fgame%2Fpenguin-diner"},
+  ];
 
   return (
     <div className="games">
-      <h1>Doğum Günü Mesajları</h1>
       <div className="games-list">
         {items.map((item, index) => (
-          <div key={index} className="message">
-            <iframe  src={item.game} width="900" height="510" scrolling="none" frameborder="0" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen" allowfullscreen="allowfullscreen"></iframe>
-          </div>
+          <Accordion key={index}>
+            <AccordionSummary
+              aria-controls={`panel${index}-content`}
+              id={`panel${index}-header`}
+            >
+              <Typography>{item.gameName}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <iframe
+                src={item.game}
+                width="900"
+                height="510"
+                scrolling="none"
+                frameBorder="0"
+                webkitAllowFullScreen
+                mozAllowFullScreen
+                allowFullScreen
+              ></iframe>
+            </AccordionDetails>
+          </Accordion>
         ))}
       </div>
     </div>
--- src/Pages/Musics.jsx
+++ src/Pages/Musics.jsx
@@ -4,6 +4,24 @@ const Musics = () => {
   const items = 
   [
     {music:"https://open.spotify.com/embed/track/52cpPh4oiM8aWu2pk4tImr?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/28WZ17sqo5faDvvjr8RZEJ?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/21Rc0BTM9FJQaN1WFmYMaV?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/3u2hBezoNDJdMfbrKYdMwL?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/4tXVu4pS8ip7Tt4X9CpAxI?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/4reNFAKWdv4VlwCQDDmXVP?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/5Ju2DX6SNF2b1heNKchwiY?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/5McqWW9E28hejw11vXMmxE?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/3BvmlFtOAxuZvGDCrUyqsj?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/4ViJpCfojsF6yncsDtXGFW?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/5vgUf8xUqFG5XcvdRsqXxd?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/3ZFCLd6WmmuJjNqLxvKVRG?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/65UH6G3VdwHicwrZ2CnYHb?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/4LXLtYkX7JpdgZgKxdKF4k?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/3ajYmPDtB3P7JTPQ64Dr7P?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/0pGQ7ie0yvg79MrIlR0wDt?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/2aCd0Ulx6YIIhRAbhQnK3B?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/4sFGNz4MYpGoz53ZGCwsiE?utm_source=generator"},
+    {music: "https://open.spotify.com/embed/track/2FEWcWHnDmGD6WSqpW4VYu?utm_source=generator"},
   ]
 
   return (
@@ -12,7 +30,7 @@ const Musics = () => {
       <div className="musics-list">
         {items.map((item, index) => (
           <div key={index} className="message">
-            <iframe style={{borderRadius:"12px"}} src={item.music} width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe style={{borderRadius:"12px"}} src={item.music} width="100%" height="252" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
           </div>
         ))}
       </div>
